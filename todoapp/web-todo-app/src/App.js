e224792b6f3750a991be1f01d1ae2a4bdd93f3ca
aware <aware9758@gmail.com>
2023-06-27 16:25:17 +0200

    here

--- todoapp/web-todo-app/src/App.js
+++ todoapp/web-todo-app/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
-import SignInPage from "./auth/SignInPage";
-
+import Main from "./components/Main";
 export default function App() {
   return (
     <div className="title">
       <h1>Here we go ..</h1>
-      <SignInPage />
+      <Main />
     </div>
   );
 }
