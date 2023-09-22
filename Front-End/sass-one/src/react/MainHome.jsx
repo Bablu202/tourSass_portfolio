54877d73c76e3d9d135d5a488ab4ab5e3b941a0f
aware <aware9758@gmail.com>
2023-09-22 12:09:27 +0200

    here

--- Front-End/sass-one/src/react/MainHome.jsx
+++ Front-End/sass-one/src/react/MainHome.jsx
@@ -4,9 +4,36 @@ export default function MainHome() {
   return (
     <div>
       <section className="section-about">
-        <h2 className="heading-secondary">
-          Exiciting tours for adventurous people
-        </h2>
+        <div className="u-center-text u-marginB-8rem">
+          <h2 className="heading-secondary">
+            Exiciting tours for adventurous people
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-1of2">
+            <h3 className="heading-tertiary u-marginB-s">
+              there is more in Nature to explore
+            </h3>
+            <p className="paragraph">
+              Ipsum dolor sit amet consectetur, adipisicing elit. Omnis
+              excepturi animi aut molestias dolorem consectetur cupiditate
+              quidem cum illum blanditiis, est dicta. Fuga, cupiditate corrupti
+              minima hic culpa architecto reprehenderit.
+            </p>
+            <h3 className="heading-tertiary">
+              lets do this again for a better adventures
+            </h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatem veniam ut et iusto distinctio mollitia, numquam ipsum
+              est totam voluptatum.
+            </p>
+            <a href="#home-learn" className="btn-text" id="home-learn">
+              Learn more &rarr;
+            </a>
+          </div>
+          <div className="col-1of2">Image Content</div>
+        </div>
       </section>
     </div>
   );
